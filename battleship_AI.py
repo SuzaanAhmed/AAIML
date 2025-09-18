9334c02e80d1379be097538c16ff73033ed2416c
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-09-18 11:56:59 +0530

    Battleship board and move selection

--- battleship_AI.py
+++ battleship_AI.py
@@ -5,62 +5,35 @@ class BTS:
 
         self.size=size
 
-        self.MainShip=5
-        self.SubShip=3
-        self.TinyShip=1
+        self.ships={
+            "Main Ship":5,
+            "Sub Ship":3,
+            "Tiny Ship":1
+        }
 
-        self.MainShipTest=False
-        self.SubShipTest=False
-        self.TinyShipTest=False
+        # self.MainShip=5
+        # self.SubShip=3
+        # self.TinyShip=1
+
+        # self.MainShipTest=False
+        # self.SubShipTest=False
+        # self.TinyShipTest=False
 
     def choose_Pos(self):
         while True:
-            if self.MainShipTest and self.SubShipTest and self.TinyShipTest:
-                choice=print("Are you satisfied? Do you want to leave?")
-                if choice=='y':
-                    break
-                
-            shipName=input("Enter ship to place: MS:5,SS:3,TS:1 or R for reset: ") 
-            
-            if shipName=='MS':
-                cache=[]
-                for i in range(self.MainShip):
-                    x,y=input(int("Enter positon of ships, 5 boxes long: ")).split()
-                    cache.append(x,y)
+            for ship,shipCount in self.ships.items():
+                for i in range(shipCount):
+                    x,y=input(int(f"Enter positon for {ship}, {shipCount} boxes long: ")).split()
                     if 0<=x<self.size and 0<=y<self.size and self.player_board[x][y]==' ':
                         self.player_board[x][y]='*'
                     else:
                         print("Invalid move. Try again.")
                         i-=1
-                        continue
-            
-            elif shipName=='SS':
-                cache=[]
-                for i in range(self.SubShip):
-                    x,y=input(int("Enter positon of ships, 3 boxes long: ")).split()
-                    cache.append(x,y)
-                    if 0<=x<self.size and 0<=y<self.size and self.player_board[x][y]==' ':
-                        self.player_board[x][y]='*'
-                    else:
-                        print("Invalid move. Try again.")
-                        i-=1
-                        continue
-            
-            elif shipName=='TS':
-                cache=[]
-                for i in range(self.TinyShip):
-                    x,y=input(int("Enter positon of ships, 1 box long: ")).split()
-                    cache.append(x,y)
-                    if 0<=x<self.size and 0<=y<self.size and self.player_board[x][y]==' ':
-                        self.player_board[x][y]='*'
-                    else:
-                        print("Invalid move. Try again.")
-                        i-=1
-                        continue
-            
-            elif shipName=='reset':
-                self.player_board=[[' ' for _ in range(self.size)] for _ in range(self.size)]
+                        continue                    
 
+            reset=(input("Do you want to reset the board?(y/n)"))
+            if reset=="n":
+                break
 
     def print_playerBoard(self):
         print('-------------')
