c1d41d38bfe5b49f2c017d9dbf238aeffbc6af5e
SuzaanAhmed <SuzaanAhmed00@gmail.com>
2025-10-08 11:25:10 +0530

    Battleship board and move selection

--- battleship_AI.py
+++ battleship_AI.py
@@ -1,5 +1,5 @@
 class BTS:
-    def __init__(self,size=14):
+    def __init__(self,size=10):
         self.player_board=[[' ' for _ in range(size)] for _ in range(size)]
         self.AI_board=self.player_board
 
@@ -19,29 +19,37 @@ class BTS:
         # self.SubShipTest=False
         # self.TinyShipTest=False
 
-    def choose_Pos(self):
-        while True:
-            for ship,shipCount in self.ships.items():
-                for i in range(shipCount):
-                    x,y=input(int(f"Enter positon for {ship}, {shipCount} boxes long: ")).split()
-                    if 0<=x<self.size and 0<=y<self.size and self.player_board[x][y]==' ':
-                        self.player_board[x][y]='*'
-                    else:
-                        print("Invalid move. Try again.")
-                        i-=1
-                        continue                    
-
-            reset=(input("Do you want to reset the board?(y/n)"))
-            if reset=="n":
-                break
 
     def print_playerBoard(self):
-        print('-------------')
+        print('\n-----------------------------------------')
         for row in self.player_board:
             print('|', end='')
             for cell in row:
                 print(f" {cell} ", end='|')
-            print('\n-------------')
+            print('\n-----------------------------------------')
         print()
-            
-    
+
+
+    def choose_Pos(self):
+        while True:
+            for ship,shipCount in self.ships.items():
+                # for i in range(shipCount):
+                self.print_playerBoard()
+                x,y=map(int,input(f"Enter positon for {ship}, {shipCount} boxes long: ").split())
+                for i in range(shipCount):
+                    if 0<=x<self.size and 0<=y<self.size and self.player_board[x][y]==' ':
+                        self.player_board[x][y]='*'
+                # else:
+                #     print("Invalid move. Try again.")
+                #     i-=1
+                #     continue                    
+
+            reset=(input("Do you want to reset the board?(y/n): "))
+            if reset=="n":
+                break
+
+            self.player_board=[[' ' for _ in range(self.size)] for _ in range(self.size)]
+
+if __name__=="__main__":
+    bts=BTS()
+    bts.choose_Pos()
